747bafda78adcb31331d0934dc082998d3c408ff
Gagandeep Singh <gagan.ddun@gmail.com>
2020-06-30 22:06:00 +1000

    added basic server

--- hello.go
+++ hello.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	var i int
-	fmt.Println(i)
-	fmt.Println("Hello, world.")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
